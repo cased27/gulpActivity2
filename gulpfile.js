c230213c3ba454fcb1c4d6c17f34fad6525279f9
Casey D <caseydasilva@hotmail.com>
2021-04-08 12:13:18 -0700

    basic setup done
    im going to now convert this basic outline into a portfolio page more specific to me and my info/projects.
    i may also update bootstrap to bulma

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 const gulp = require('gulp');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
 
 gulp.task('default', function(completed) {
     console.log('hello');
     completed();
+})
+
+gulp.task('html', () => {
+    return gulp.src('index.hbs')
+        .pipe(handlebars({}, {
+            batch: ['./partials']
+        }))
+        .pipe(rename({
+            extname: '.html'
+        }))
+        .pipe(gulp.dest('./'));
 })
